4b281c26de4f2bcc12d325ec42f2e05a0e7b37c3
Kevin Modzelewski <kevmod@gmail.com>
2022-06-16 17:37:18 -0400

    Migrate aiohttp and gunicorn to single-webserver style

--- benchmarks/bm_gunicorn/run_benchmark.py
+++ benchmarks/bm_gunicorn/run_benchmark.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import os.path
 import requests
 import sys
@@ -48,23 +49,23 @@ def _bench_gunicorn(loops=3000, legacy=False):
     start = pyperf.perf_counter()
     elapsed = 0
     times = []
-    with netutils.serving(ARGV, DATADIR, ADDR):
-        requests_get = requests.get
-        for i in range(loops):
-            # This is a macro benchmark for a Python implementation
-            # so "elapsed" covers more than just how long a request takes.
-            t0 = pyperf.perf_counter()
-            requests_get("http://localhost:8000/blog/").text
-            t1 = pyperf.perf_counter()
 
-            elapsed += t1 - t0
-            times.append(t0)
-            if legacy and (i % 100 == 0):
-                print(i, t0 - start)
-        times.append(pyperf.perf_counter())
-        if legacy:
-            total = times[-1] - start
-            print("%.2fs (%.3freq/s)" % (total, loops / total))
+    requests_get = requests.get
+    for i in range(loops):
+        # This is a macro benchmark for a Python implementation
+        # so "elapsed" covers more than just how long a request takes.
+        t0 = pyperf.perf_counter()
+        requests_get("http://localhost:8000/blog/").text
+        t1 = pyperf.perf_counter()
+
+        elapsed += t1 - t0
+        times.append(t0)
+        if legacy and (i % 100 == 0):
+            print(i, t0 - start)
+    times.append(pyperf.perf_counter())
+    if legacy:
+        total = times[-1] - start
+        print("%.2fs (%.3freq/s)" % (total, loops / total))
     return elapsed, times
 
 
@@ -75,6 +76,12 @@ if __name__ == "__main__":
     from legacyutils import maybe_handle_legacy
     maybe_handle_legacy(_bench_gunicorn, legacyarg='legacy')
 
-    runner = pyperf.Runner()
-    runner.metadata['description'] = "Test the performance of gunicorn"
-    runner.bench_time_func("gunicorn", bench_gunicorn)
+    if "--worker" not in sys.argv:
+        context = netutils.serving(ARGV, DATADIR, ADDR)
+    else:
+        context = nullcontext()
+
+    with context:
+        runner = pyperf.Runner()
+        runner.metadata['description'] = "Test the performance of gunicorn"
+        runner.bench_time_func("gunicorn", bench_gunicorn)
